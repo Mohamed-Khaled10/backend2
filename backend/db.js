db57e3bdeeefec0d6251a59419f3d0faf22cd7b5
Mohamed-Khaled10 <mk2201104@tkh.edu.eg>
2025-01-14 23:41:22 +0200

    2

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,29 @@
 const sqlite= require('sqlite3')
 const db= new sqlite.Database('courts.db')
 
-const createUserTable= `CREATE TABLE IF NOT EXISTS USER (id INTEGER PRIMARY KEY AUTOINCREMENT,
+const createUserTable= `CREATE TABLE IF NOT EXISTS USER(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 name TEXT NOT NULL, 
 email TEXT NOT NULL UNIQUE,
 password TEXT NOT NULL,
-ISADMIN INT)`
+is_admin INT DEFAULT 0 CHECK (is_admin IN (0, 1))`
 
-const createCourtsTable  = `CREATE TABLE IF NOT EXISTS COURTS(
+const createCourtTable  = `CREATE TABLE IF NOT EXISTS COURT(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 name TEXT NOT NULL,
 location TEXT NOT NULL,
-price INTEGER NOT NULL,
+price REAL NOT NULL,
 phonenum TEXT NOT NULL,
 court_amenities TEXT NOT NULL)`
+
+const createBookingTable  = `CREATE TABLE IF NOT EXISTS BOOKING(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+user_id INT NOT NULL,
+court_id INT NOT NULL,
+time TEXT NOT NULL,
+date TEXT NOT NULL,
+FOREIGN KEY (user_id) REFERENCES USER(id),
+FOREIGN KEY (court_id) REFERENCES COURT(id))`
+
+module.exports={db,createUserTable,createCourtTable,createBookingTable}
 
